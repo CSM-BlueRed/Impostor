215a29acee50de97d9acc91e4dbf7dfbdb120610
CSM-BlueRed <89811976+CSM-BlueRed@users.noreply.github.com>
2022-05-26 13:46:32 +0200

    📌 Update: Make a gateway to increase securiy

--- impostor.py
+++ impostor.py
@@ -4,6 +4,7 @@ import os
 from pystyle import *
 import marshal
 import base64
+import random
 
 class Impostor():
 
@@ -14,7 +15,7 @@ class Impostor():
         return f'eval(eval({formated!r}))' if evalCode else f'eval({formated!r})'
 
     hidedText = 'GG! You deobfuscated the code! obfuscated by Impostor'
-    exceptionCode = 'input("Don\'t try to skid Impostor obfuscation !")'
+    exceptionCode = 'input("Don\'t try to skid Impostor obfuscation !");exit(0)'
     gitLink = 'https://github.com/CSM-BlueRed/Impostor'
 
     infos = {
@@ -22,19 +23,40 @@ class Impostor():
         '__madeBy__': 'Impostor',
         '__git__': gitLink,
     }
+    gatewayKey = random.randint(0, 10000)
+    def Gateway() -> str:
+        _globals = globals()['__globals']
+        obj = globals()['__selfObject__']
+        interpreterObj = globals()['__InterpreterObject__']
+        module = globals()['__module']
+        key = globals()['__key__']
+        code = interpreterObj.code['bytes']
+        def send_(*args):
+            for arg in args: exec(arg)
+        obj.executed = True
+        __import__('builtins').send = send_
+        return ((key * 8 / 1.5), code)
     comment = 'Obfuscated with Impostor'
     encCommend = ' + '.join(f'chr({char})' for char in [ord(char_) for char_ in comment])
     checkInfos = ' and '.join(f'globals().get("{key}") == {value!r}' for key, value in infos.items()) + \
         f' and ("# " and {encCommend}) in open(__import__("os").path.abspath(__file__), "r", encoding = "utf-8").read() '
     interpreterClass = """
 class Interpreter():
-  def __init__(self, code: str, layersFunction: bytes, module, globals_) -> None: self.__module = module;self.layersFunction = layersFunction;self.__globals = globals_;self.code = {'bytes': code, 'str': str(code)}
-  def Run(self) -> None: decoder = self.__getobject__(); exec(MARSHALMODULE.loads(decoder), {'__selfObject__': self, '__module': self.__module, '__sr_m': MARSHALMODULE, '__globals': self.__globals})
+  def __init__(self, code: str, layersFunction: bytes, module, globals_, backend: bytes = b'') -> None: self.__module = module;self.layersFunction = layersFunction;self.__globals = globals_;self.code = {'bytes': code, 'str': str(code)}; self.__backend = backend
+  def __tunnel(self) -> Gateway: return Gateway(self.__backend, GATEWAYKEY, __module = self.__module, __globals = self.__globals, interpreter = self)
+  def Run(self) -> None: decoder = self.__getobject__(); gate = self.__tunnel().Pass();exec(eval(MARSHALMODULE.loads(decoder), {'__selfObject__': self, '__module': self.__module, '__sr_m': MARSHALMODULE, '__globals': self.__globals, 'gate': gate}))
   def __getobject__(self) -> object: func = self.layersFunction; return self.__module.b64decode(func)
+"""[1:-1].replace('MARSHALMODULE', Alt('__import__("marshal")')).replace('GATEWAYKEY', str(gatewayKey))
+
+    gatewayClass = """
+class Gateway():
+  def __init__(self, way: bytes, key: int, **ext) -> None: self.way = way;self.key = key; self.module__ = ext.get('__module', None);self.__globals = ext.get('__globals', None);self.__module = ext.get('__module', None); self.__interpreter = ext.get('interpreter', None)
+  def Pass(self): exec(MARSHALMODULE.loads(self.module__.b16decode(self.way)), {'__selfObject__': self, '__key__': self.key, '__module': self.module__, '__globals': self.__globals, '__InterpreterObject__': self.__interpreter}); return self
 """[1:-1].replace('MARSHALMODULE', Alt('__import__("marshal")'))
 
     def RemoveLayers() -> str:
         _globals = globals()['__globals']
+        if not globals().get('gate'): return
         obj = globals()['__selfObject__']
         module = globals()['__module']
         code = obj.code['bytes']
@@ -43,7 +65,6 @@ class Interpreter():
         code = module.b32decode(code)
         code = module.b16decode(code)
         code = globals()['__sr_m'].loads(code)
-        exec(code, _globals)
         return code
         
 
@@ -69,9 +90,14 @@ if not ({Impostor.checkInfos}): {Impostor.exceptionCode}
 
 # {Impostor.comment}
 
+{Impostor.gatewayClass}
 {Impostor.interpreterClass}
 
-Interpreter({code__!r}, {base64.b64encode(Impostor.serializer.dumps(Impostor.RemoveLayers.__code__))!r}, {Impostor.Alt('__import__("base64")')}, globals()).Run()
+Interpreter({code__!r},
+            {base64.b64encode(Impostor.serializer.dumps(Impostor.RemoveLayers.__code__))!r},
+            {Impostor.Alt('__import__("base64")')}, globals(),
+            {base64.b16encode(Impostor.serializer.dumps(Impostor.Gateway.__code__))!r}
+).Run()
 """[1:-1]
         return code_
 
